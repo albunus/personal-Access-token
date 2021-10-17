377a436a26495fb36f3e3b89ade1798ce18fa50b
albunus <albunus.nyalita@student.moringaschool.com>
2021-10-17 19:35:50 +0300

    generate pipe date

--- Github-search/src/app/app.module.ts
+++ Github-search/src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import{HttpClientModule} from '@angular/common/http';
 import { LandingPageComponent } from './landing-page/landing-page.component';
-import { ProfilePageComponent } from './profile-page/profile-page.component'
+import { ProfilePageComponent } from './profile-page/profile-page.component';
+import { DatePipe } from './date.pipe'
 
 @NgModule({
   declarations: [
     AppComponent,
     LandingPageComponent,
-    ProfilePageComponent
+    ProfilePageComponent,
+    DatePipe
   ],
   imports: [
     BrowserModule,
