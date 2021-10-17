f8b7463d6550d6ca63c227def1c1221701ead58d
albunus <albunus.nyalita@student.moringaschool.com>
2021-10-17 19:11:25 +0300

    import httClientModule

--- Github-search/src/environments/environment.ts
+++ Github-search/src/environments/environment.ts
@@ -3,9 +3,12 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  // AccessUrl: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
 };
 
+
+// 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
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
