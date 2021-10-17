0f9b446a48d63e80c1c98be000b79df2c8d86ee1
albunus <albunus.nyalita@student.moringaschool.com>
2021-10-17 19:15:54 +0300

    update my service class

--- Github-search/src/app/service/user/user.service.ts
+++ Github-search/src/app/service/user/user.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -5,5 +6,38 @@ import { Injectable } from '@angular/core';
 })
 export class UserService {
 
-  constructor() { }
+  private username: string;
+  private clientid = "053e4f137946b59bf6fb";
+  private clientsecret = "68cec4984fc7163541c43cd828af13c020e786fb";
+
+  constructor(private http: HttpClient) {
+    console.log("service is now ready!");
+    this.username = "saltbush";
+  }
+  getprofileInfo() {
+    return this.http.get(
+      "https://api.github.com/users/" +
+        this.username +
+        "?client_id=" +
+        this.clientid +
+        "&client_secret=" +
+        this.clientsecret
+    );
+  }
+  getProfileRepos() {
+    return this.http.get(
+      "https://api.github.com/users/" +
+        this.username +
+        "/repos?client_id=" +
+        this.clientid +
+        "&client_secret=" +
+        this.clientsecret
+    );
+  }
+  updateusername(username: string) {
+    this.username = username;
+  }
+  updateProfile(username: string) {
+    this.username = username;
+  }
 }
