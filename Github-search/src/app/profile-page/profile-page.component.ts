2abcb8c6903066beb4f91afbc62be822e6dc3056
albunus <albunus.nyalita@student.moringaschool.com>
2021-10-17 19:31:57 +0300

    update my profile-page

--- Github-search/src/app/profile-page/profile-page.component.ts
+++ Github-search/src/app/profile-page/profile-page.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { UserService } from '../service/user/user.service';
 @Component({
   selector: 'app-profile-page',
   templateUrl: './profile-page.component.html',
@@ -7,9 +7,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfilePageComponent implements OnInit {
 
-  constructor() { }
+  profile: any;
+  repos: any;
+  username!: string;
 
-  ngOnInit(): void {
+  constructor(private userService:UserService) {
+
+    this.userService.updateProfile(this.username);
+    this.userService.getprofileInfo().subscribe(Profile => {
+      console.log(Profile);
+      this.profile = Profile;
+    });
+    this.userService.getProfileRepos().subscribe(repos => {
+      console.log(repos);
+      this.repos = repos;
+    });
+  }
+  findProfile() {
+    this.userService.updateProfile(this.username);
+    this.userService.getprofileInfo().subscribe(Profile => {
+      console.log(Profile);
+      this.profile = Profile;
+    });
+    this.userService.getProfileRepos().subscribe(repos => {
+      console.log(repos);
+      this.repos = repos;
+    });
   }
 
+  ngOnInit() {}
 }
+
